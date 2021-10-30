14944005bc9bfc6f39afe49fd44157fe3bffccaa
rinanasir <rinanasir25@gmail.com>
2021-10-30 23:13:17 +0600

    final commit

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -9,7 +9,7 @@ const AddService = () => {
     const onSubmit = data => {
         console.log(data);
 
-        axios.post('http://localhost:5000/services', data)
+        axios.post('https://murmuring-tundra-07797.herokuapp.com/services', data)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('added successfully');
